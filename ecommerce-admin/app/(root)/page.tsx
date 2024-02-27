12f608e80f483355fdb77bc6b416cb4cc98c7e98
Meekaylah <kaylahusanga@gmail.com>
2024-02-27 02:27:26 +0100

    created root route group

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <>
+        <p>Hello Admin Dashboard</p>
+        <div className="p-4">
+            <Button size="default" variant="destructive">Delete</Button>
+        </div>
+        </>
+    );
+}  
