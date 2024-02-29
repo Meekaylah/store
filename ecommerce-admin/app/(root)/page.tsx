9528707762cde13abb966b9595f49aa292c9420f
Meekaylah <kaylahusanga@gmail.com>
2024-02-29 15:36:53 +0100

    Added Clerk Uzer Button

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
     return (
-        <>
-        <p>Hello Admin Dashboard</p>
         <div className="p-4">
-            <Button size="default" variant="destructive">Delete</Button>
+            <UserButton afterSignOutUrl="/"/>
         </div>
-        </>
     );
 }  
+
+export default SetupPage;
