c3738b560864fe93b83bfd23f58c0c6776b1cf9e
Meekaylah <kaylahusanga@gmail.com>
2024-03-06 22:08:52 +0100

    Made create store component global

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
-import "./globals.css";
+
+import { ModalProvider } from "@/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard", 
@@ -18,7 +21,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ModalProvider/>
+        {children}
+      </body>
     </html>
     </ClerkProvider>
   );
--- ecommerce-admin/providers/modal-provider.tsx
+++ ecommerce-admin/providers/modal-provider.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 
+import { StoreModal } from "@/components/modals/store-modal";
+
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
 
@@ -12,4 +14,10 @@ export const ModalProvider = () => {
     if (!isMounted) {
         return null;
     }
+
+    return (
+        <>
+        <StoreModal/>
+        </>
+    )
 }
