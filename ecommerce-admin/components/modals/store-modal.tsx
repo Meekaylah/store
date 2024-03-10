86f70ef352d8426c1d6a1c73728441fd1ac7874f
Meekaylah <kaylahusanga@gmail.com>
2024-03-10 21:14:20 +0100

    not too sure what i'm doing it here but i want to figure it out

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -1,16 +1,26 @@
 "use client"
 
-import * as z from "zod"
+import {z} from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal"
 
 const formSchema = z.object({
-    
+    name: z.string().min(1),
+
 })
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        defaultValues: {
+            name: "",
+        },
+        resolver: zodResolver(formSchema),
+    })
 
     return (
         <Modal
